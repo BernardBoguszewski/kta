a26c6b37d3526e4c60d1c2f47f5e1b9f7ecb9776
Łukasz Puła <lukasz.pula@britenet.com.pl>
2017-07-13 17:57:55 +0200

    Init project with MongoDB

---
+++ src/main/java/pl/com/britenet/kta/KtaApplication.java
@@ -0,0 +1,12 @@
+package pl.com.britenet.kta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtaApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/com/britenet/kta/KtaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.com.britenet.kta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KtaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

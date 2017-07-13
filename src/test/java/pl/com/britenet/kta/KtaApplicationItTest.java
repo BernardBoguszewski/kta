596faa1d64b9553e773553d45a14b53863214a4c
Łukasz Puła <lukasz.pula@britenet.com.pl>
2017-07-13 21:00:01 +0200

    Add Spock to project. Divide Api, Unit, integration tests

--- src/test/java/pl/com/britenet/kta/KtaApplicationItTest.java
+++ src/test/java/pl/com/britenet/kta/KtaApplicationItTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KtaApplicationTests {
+public class KtaApplicationItTest {
 
 	@Test
 	public void contextLoads() {
